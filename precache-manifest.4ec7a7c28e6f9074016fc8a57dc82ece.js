eef552ce911ae37786d855cee6495e14747a1191
Shankar Mattaparthi <shankar.mattaparthi@supplier.io>
2025-01-29 21:46:07 +0530

    Updates

---
+++ precache-manifest.4ec7a7c28e6f9074016fc8a57dc82ece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130c13e49a3bfd19755a42f7da5d4ac4",
+    "url": "/reactpoc/index.html"
+  },
+  {
+    "revision": "7c6322c631958a6f6bc2",
+    "url": "/reactpoc/static/css/main.7ee1d415.chunk.css"
+  },
+  {
+    "revision": "3b1642e77375fa202443",
+    "url": "/reactpoc/static/js/2.56f905a4.chunk.js"
+  },
+  {
+    "revision": "bed8c513175015c45fa1dc1f4df06a9f",
+    "url": "/reactpoc/static/js/2.56f905a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6322c631958a6f6bc2",
+    "url": "/reactpoc/static/js/main.43a3efa3.chunk.js"
+  },
+  {
+    "revision": "80bf7e07c22ee2f298ba",
+    "url": "/reactpoc/static/js/runtime-main.835ae2e2.js"
+  }
+]);
