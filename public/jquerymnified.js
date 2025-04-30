d70fec7968619b25b9f40bd05a4baf4d4558e34f
nagashankarui <129832515+nagashankarui@users.noreply.github.com>
2025-04-30 14:26:36 +0530

    Add files via upload

---
+++ public/jquerymnified.js
@@ -0,0 +1,1 @@
+$(function(){$("a").click(function(e){e.preventDefault();alert("Link clicked!")});$("#btn").on("click",function(){$("p").toggleClass("highlight")})});
---
+++ public/mnified.jsx
@@ -0,0 +1,1 @@
+import React from"react";export default function App(){return<div><h1>Hello</h1><p>Welcome to minified React!</p></div>}
